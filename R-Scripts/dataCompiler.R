8b58dbf0a63f23a90fa0095eb21715b0df209a00
Preston Bruce <pb.itoldyouso@gmail.com>
2018-07-05 11:18:28 -0500

    Code Update

--- R-Scripts/dataCompiler.R
+++ R-Scripts/dataCompiler.R
@@ -8,6 +8,10 @@ library(effects)
 library(sjmisc)
 bugTables <- readxl::read_xlsx("R-Formated Data/Sorted Master Bug Data Set.xlsx")
 ecoTables <-readxl::read_xlsx("R-Formated Data/County Eco Map (4 Groupings).xlsx")
+wingTable <-readxl::read_xlsx("R-Formated Data/Species Hosts and Wingspan.xlsx")
+
+#mergeBugTable <- merge(bugTables,wingTable, by. = "SPECIES" )
+
 
 eco6Anova<-aov(NumberOfSamples ~ NumberOfSpecies * EcologicalRegionSixCount,data = ecoTables)
 eco4Anova<-aov(NumberOfSamples ~ NumberOfSpecies * EcologicalRegionFourCount,data = ecoTables)
@@ -15,17 +19,33 @@ eco6Sum<-summary(eco6Anova)
 eco4Sum<-summary(eco4Anova)
 
 testModel <- lmer(QUANTITY ~ COUNTY*(1|SPECIES), data = bugTables)
-ecoModel <- lmer(NumberOfSamples ~ NumberOfSpecies *(1|EcologicalRegionSixCount),data = ecoTables)
+elaModel<-lmer(NumberOfSpecies~Elavation*(1|EcologicalRegionFourCount), data = ecoTables)
+ecoModel <- lmer(NumberOfSamples ~ NumberOfSpecies *(1|EcologicalRegionFourCount),data = ecoTables)
+areaModel <- lmer(NumberOfSpecies ~ Area * (1|EcologicalRegionFourCount), data = ecoTables)
+latLongModel<-lmer(NumberOfSpecies ~ Latitude+Logitude*(1|EcologicalRegionFourCount), data = ecoTables)
+tempModel<-lmer(NumberOfSpecies ~ AverageLowTemp+AverageHiTemp*(1|EcologicalRegionFourCount), data = ecoTables)
+rainModel<-lmer(NumberOfSpecies ~ AveragePerciptation*(1|EcologicalRegionFourCount), data = ecoTables)
+
+summary(elaModel)
+summary(latLongModel)
+summary(areaModel)
+summary(tempModel)
+summary(rainModel)
+print(elaModel,correlation=TRUE)
+print(latLongModel, correlation=TRUE)
+print(areaModel,correlation=TRUE)
 summary(ecoModel)
 summary(testModel)
 print(testModel, correlation=TRUE)
+
+#ggplot(fortify(elaModel), aes())
 
 sjp.lmer(testModel, y.offset = .4)
 baseGraph<-ggplot(bugTables, aes(COUNTY, QUANTITY, color =SPECIES)) + geom_point() + theme(axis.text.x = element_text(angle = 90, vjust = .6, hjust = 1))
 
 yearGraph<-ggplot(bugTables, aes(YEAR, QUANTITY, color =SPECIES)) + geom_point() + theme(axis.text.x = element_text(angle = 90, vjust = .6, hjust = 1))
 
-
+ 
 
 #testAnova<-aov(QUANTITY ~ COUNTY*SPECIES, data = bugTables)
 #summary(testAnova)
